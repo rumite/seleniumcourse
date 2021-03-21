90fb70f86b167ab1465774c150fff18abfdb0e5c
rumite <bakhtiyarowtn@gmail.com>
2021-03-21 23:31:44 +0300

    Initial commit

---
+++ src/main/java/FirstTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    @Test
+    public void testSearchGoogle(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://google.com");
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+
+        searchBox.sendKeys("ChromeDriver");
+
+        searchBox.submit();
+        driver.navigate().to("https:selenium.dev");
+        driver.quit();
+    }
+
+}
